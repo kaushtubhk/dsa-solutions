761d20c12b9379e98c55a9434407e8214109e5b1
Kaushtubh Kumar <kaushtubhkumar6@gmail.com>
2026-06-25 16:01:49 +0530

    add two sum solution - hashmap pattern

---
+++ arrays/two_sum.cpp
@@ -0,0 +1,20 @@
+// Problem: Two Sum - LeetCode #1
+// Pattern: Hashmap
+// Difficulty: Easy
+
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> twoSum(vector<int>& nums, int target) {
+    unordered_map<int,int> mp;
+    
+    for(int i = 0; i < nums.size(); i++) {
+        int complement = target - nums[i];
+        
+        if(mp.count(complement))
+            return {mp[complement], i};
+            
+        mp[nums[i]] = i;
+    }
+    return {};
+}
