ead202bdbdc7a2c1eee6ebf65ca92c9b530559c3
Kaushtubh Kumar <kaushtubhkumar6@gmail.com>
2026-06-28 15:45:24 +0530

    product of array except self

---
+++ arrays/productofarrayexceptself.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+
+        vector<int> left(n,1); // create array of left 
+        for(int i = 1; i < n ; i++){
+            left[i] = left[i-1] * nums[i-1];
+        }
+        vector<int> right(n,1); // create array of right
+        for(int i = n-2 ; i>=0 ; i--){ // reverse loop use krenge 
+            right[i] = right[i+1] * nums[i+1];
+        }
+        vector<int> answer(n);
+        for(int i = 0 ; i < n ; i++){
+            answer[i] = left[i] * right[i];
+        }
+        return answer;
+    }
+};
+//TC = O(n)
+//SC = O(n)
