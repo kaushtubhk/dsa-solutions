fe0ebd75ecd8d1075dd8cc98ea8fd0c25cfdfb0e
Kaushtubh Kumar <kaushtubhkumar6@gmail.com>
2026-07-03 13:00:34 +0530

    sort colors

---
+++ arrays/sortcolors.cpp
@@ -0,0 +1,24 @@
+class Solution { //use DUTCH NATIONAL FLAG ALGO
+public:
+    void sortColors(vector<int>& nums) {
+        int low = 0;
+        int mid = 0;
+        int high = nums.size() - 1;
+
+        while(mid <= high){
+            if(nums[mid] == 0){
+                swap(nums[low] , nums[mid]);
+                low++;
+                mid++;
+            }else if(nums[mid] == 1){
+                mid++;
+            }else{
+                swap(nums[mid] , nums[high]);
+                high--;
+            }
+        }
+        
+    }
+};
+//TC = O(n)
+//SC = O(1)
