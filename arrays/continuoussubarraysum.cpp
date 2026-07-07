6e456d46b3c393eafb4bcbe098ff27a27ca858bd
Kaushtubh Kumar <kaushtubhkumar6@gmail.com>
2026-07-07 19:00:12 +0530

    continuous subarray sum

---
+++ arrays/continuoussubarraysum.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        int n = nums.size();
+        unordered_map<int,int> mp;
+        mp[0] = -1;
+        int prefix = 0;
+        for(int i = 0 ; i < n ; i++){
+            prefix += nums[i];
+            int rem = prefix % k;
+            if(mp.count(rem)){
+                if(i - mp[rem] >= 2){ //check length of subarray more than 2 or not
+                    return true;
+            
+
+                }else{
+                    mp[rem] = i; //store rem
+                }
+            }
+        }
+        return false;
+
+        
+    }
+};
+//TC = O(n)
+//SC = O(n)
