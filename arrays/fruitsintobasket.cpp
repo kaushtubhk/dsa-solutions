ecd328b1a7b19c377354ab8bcbdcc8ed7e15aff4
Kaushtubh Kumar <kaushtubhkumar6@gmail.com>
2026-07-26 18:23:43 +0530

    fruits into basket

---
+++ arrays/fruitsintobasket.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        int n = fruits.size();
+        int maxLen = 0;
+        int left = 0;
+        unordered_map<int, int> mp;
+
+        for (int right = 0; right < n; right++) {
+            mp[fruits[right]]++;
+
+            while (mp.size() > 2) {
+                mp[fruits[left]]--;
+
+                if (mp[fruits[left]] == 0) {
+                    mp.erase(fruits[left]);
+                }
+
+                left++;
+            }
+
+            maxLen = max(maxLen, right - left + 1);
+        }
+
+        return maxLen;
+    }
+};
