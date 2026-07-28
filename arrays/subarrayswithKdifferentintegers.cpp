335296551df7baf0ad84728078b9e8303dc2f69a
Kaushtubh Kumar <kaushtubhkumar6@gmail.com>
2026-07-28 16:48:50 +0530

    subarrays with k different integers

---
+++ arrays/subarrayswithKdifferentintegers.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int atMost(vector<int>& nums, int k){
+        unordered_map<int,int> freq ;
+        int n = nums.size();
+        int left = 0;
+        int count = 0;
+
+        for(int right = 0 ; right < n ; right++){
+            freq[nums[right]]++;
+
+            while(freq.size() > k){
+                freq[nums[left]]--;
+                if(freq[nums[left]] == 0){
+                    freq.erase(nums[left]);
+                    
+                }
+                left++ ;
+
+            }
+            count += right - left + 1 ;
+        }
+        return count ;
+    }
+    int subarraysWithKDistinct(vector<int>& nums, int k) {
+        return atMost(nums , k) - atMost(nums, k-1);
+        
+    }
+};
