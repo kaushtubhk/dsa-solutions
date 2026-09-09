71562bb7b1a053123ff4f82325bf76ecb6efef51
Kaushtubh Kumar <kaushtubhkumar6@gmail.com>
2026-09-10 02:33:46 +0530

    course schedule

---
+++ graphs/courseschedule.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(numCourses) ;
+        vector<int> indegree(numCourses,0) ;
+
+        for(auto p : prerequisites){
+            int courses = p[0] ;
+            int prerequisites = p[1] ;
+
+            adj[prerequisites].push_back(courses) ;
+            indegree[courses]++ ;
+        }
+        queue<int> q ;
+        for(int i = 0 ; i < numCourses ; i++){
+            if(indegree[i] == 0){
+                q.push(i) ;
+            }
+        }
+        int count = 0 ;
+
+        while(!q.empty()){
+            int u = q.front() ;
+            q.pop() ;
+            count++ ;
+            for(int v : adj[u]){
+                indegree[v]-- ;
+                if(indegree[v] == 0){
+                    q.push(v);
+                }
+            }
+        }
+        return count == numCourses ;        
+    }
+};
