6c50c35ebe8b9ba6c20a2c4fe91181f0beac44d2
Kaushtubh Kumar <kaushtubhkumar6@gmail.com>
2026-09-11 20:32:26 +0530

    reconstruct itinenary

---
+++ graphs/reconstructitinenary.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    unordered_map<string> , multiset<string>> adj ;
+    vector<string> ans ;
+    void dfs(string airport){
+        while(!adj[airport].empty()){
+            string next = *adj[airport].begin() ;
+            adj[airport].erase(adj[airport].begin());
+            dfs(next) ;
+        }
+        ans.push_back(airport) ;
+    }
+    vector<string> findItinerary(vector<vector<string>>& tickets) {
+        for(auto &ticket : tickets){
+            adj[ticket[0]].insert(ticket[1]);
+        }
+        dfs("JFK") ;
+        reverse(ans.begin() , ans.end()) ;
+
+        return ans ;
+        
+    }
+};
